be4296ec773d6592baab360ce7a741d96e109261
Peter Huson <peter@huson.com>
2019-04-19 03:45:24 -0400

    dead simple startup script

--- master_run.sh
+++ master_run.sh
@@ -2,8 +2,10 @@
 ### Display Run Qt App
 export ROTATION=90
 . setqt5env
-/root/fsae-apollo/build/QtE-Demo -qws
+screen -S qtscreen /root/fsae-apollo/build/QtE-Demo -qws 
 
 ### Run LEDs 
-python /root/fsae-apollo/hardware/leds.py
+screen -S leds python /root/fsae-apollo/hardware/leds.py
 
+### Run Serial Can Bus adapter
+# screen -S canbus 
