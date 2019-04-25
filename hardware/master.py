14dbf7e7b39443124597cbc496f22af64a822175
Peter Huson <peter@huson.com>
2019-04-25 01:48:59 -0400

    Master is in py3

--- hardware/master.py
+++ hardware/master.py
@@ -20,7 +20,7 @@ while (True):
     # NB: for PySerial v3.0 or later, use property `in_waiting` instead of function `inWaiting()` below!
     if (serial_port.inWaiting()>0): #if incoming bytes are waiting to be read from the serial input buffer
         data_str = serial_port.read(serial_port.inWaiting()).decode('ascii') #read the bytes and convert from binary array to ASCII
-        print(data_str, end='') #print the incoming string without putting a new-line ('\n') automatically after every print()
+        print(data_str) #print the incoming string without putting a new-line ('\n') automatically after every print()
     #Put the
 
 
