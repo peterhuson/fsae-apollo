4a5f6b71f74695029361993241aa77d0c9a59dc1
Peter Huson <peter@huson.com>
2019-04-28 18:47:32 -0400

    Stencil code for serial link

--- hardware/master.py
+++ hardware/master.py
@@ -1,4 +1,5 @@
 import serial as s
+import sys, traceback, os, time
 from leds import LEDs
  
 class Master:
@@ -6,47 +7,70 @@ class Master:
         self.l = LEDs()
         print("got an LED handler!")
 
-        serial_port = s.Serial('/dev/ttyAMA3', 9600, timeout=1) # Default Serial Baud rate is 9600
-        print(serial_port.name)
-        serial_port.write(b'+++')
-        ret = serial_port.readline()
+        self.serial_port = s.Serial('/dev/tty.Bluetooth-Incoming-Port', 9600, timeout=1) # Default Serial Baud rate is 9600
+        
+        self.serial_port = s.Serial('/dev/ttyAMA3', 9600, timeout=1) # Default Serial Baud rate is 9600
+        self.serial_port.write(b'+++')
+        ret = self.serial_port.readline()
         print(ret)
-        serial_port.write(b'AT+C=18') # Set CanBus rate to 1Mb/s
-        ret = serial_port.readline()
+        self.serial_port.write(b'AT+C=18') # Set CanBus rate to 1Mb/s
+        ret = self.serial_port.readline()
         print(ret)
-        serial_port.write(b'AT+Q')
-        ret = serial_port.readline()
+        self.serial_port.write(b'AT+Q')
+        ret = self.serial_port.readline()
         print(ret)
 
         try:
             self.run()
         except KeyboardInterrupt:
-            print "Shutdown requested...exiting"
             self.shutdown()
         except Exception:
             traceback.print_exc(file=sys.stdout)
-        sys.exit(0)
+            self.shutdown()
 
     def run(self):
         
+        self.fifo_path = '/tmp/myfifo2'
 
-        fifo_path = '/tmp/myfifo2'
-        os.mkfifo(fifo_path)
-        fifo_write = open(fifo_path, 'w')
-        fifo_write.close()
+        # FIFO Pipe can only be opened non-blocking when something is reading it
+        # try:
+        #     os.mkfifo(self.fifo_path)
+        # except OSError, e:
+        #     print "Failed to create FIFO: %s" % e
+        # else:
+        #     fifo = open(self.fifo_path, os.O_NONBLOCK)
+        #     print("Opened fifo")
+        #     fifo.close() 
+        #     os.remove(filename)
+        #     # pass
 
-        # while (True):
-        #     # NB: for PySerial v3.0 or later, use property `in_waiting` instead of function `inWaiting()` below!
-        #     if (serial_port.inWaiting()>0): #if incoming bytes are waiting to be read from the serial input buffer
-        #         data_str = serial_port.read(serial_port.inWaiting()).decode('ascii') #read the bytes and convert from binary array to ASCII
-        #         print(data_str) #print the incoming string without putting a new-line ('\n') automatically after every print()
+        # fifo = open(self.fifo_path, 'w')
+
+
+        while (True):
+            if (self.serial_port.inWaiting()>0): #if incoming bytes are waiting to be read from the serial input buffer
+                data_str = self.serial_port.read(self.serial_port.inWaiting())
+                print(data_str) 
                 
-        #     time.sleep(0.01)
+                address = data_str[:4] # Get the first 4 bytes of data
+                print(address)
+
+                value = data_str[4:] # Get everything after the first 4 bytes
+                print(value)
+
+                if address == 0x118:
+                    print("sending {} to leds".format(value))
+                    l.displayRPM(value)
+
+                
+            time.sleep(0.01)
+            print("in read loop")
             #Put the
 
 
     def shutdown(self):
-        os.remove(fifo_path)
+        print("Shutdown heard...exiting")
+        os.remove(self.fifo_path)
 
 
 if __name__ == '__main__':
