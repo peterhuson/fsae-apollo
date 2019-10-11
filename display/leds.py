a25c85c0f62f0a391676361578811c8338953a1b
Mark Lavrentyev <lavrema@outlook.com>
2019-10-11 15:16:01 -0400

    Add led control framework

--- display/leds.py
+++ display/leds.py
@@ -1,11 +1,17 @@
 from display import params
 
+
 class LEDController:
-    def __init__(self):
-        self.num_steps = params.NUM_LEDS // 2
+    def __init__(self, num_leds, data_pin, clock_pin):
+        # TODO: intialize the led strip here
+        self.num_steps = num_leds // 2
         self.step_width = (params.SHIFT_RPM - params.LED_MIN_RPM) // self.num_steps
 
-    def display_rpm(self):
+    def get_led_color(self, led_id):
         ...
 
-    def
+    def display_rpm(self, rpm):
+        ...
+
+    def flash_color(self, color, duration):
+        ...
