0aadd94989d87708db3a9b1a561dac397ee71fe5
Mark Lavrentyev <lavrema@outlook.com>
2019-10-12 23:40:51 -0400

    Clean out my own master branch to merge real one in

--- dataTransfer/params.py
+++
@@ -1,5 +0,0 @@
-from dataTransfer.dataProcessing import units
-
-# Car constants
-WHEEL_RADIUS = 8 * units.inch
-REDLINE_RPM = 13500 * units.rpm
--- main.sh
+++
@@ -1,5 +0,0 @@
-# Run steering wheel
-
-# args: numLEDs, optional use debug data?
-# pipe logs to log file
-python3 main.py 13 --debug-data > "logs/$(date +"%FT%H%M").log"
